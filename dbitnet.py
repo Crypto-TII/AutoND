220e2d1774c2a3b67dfc8db8041b0a9bc0a853e4
annahambi <anna.hambitzer@tii.ae>
2022-12-12 11:01:58 +0000

    initial commit

---
+++ dbitnet.py
@@ -0,0 +1,75 @@
+def get_dilation_rates(input_size):
+    """Helper function to determine the dilation rates of DBitNet given an input_size. """
+    drs = []
+    while input_size >= 8:
+        drs.append(int(input_size / 2 - 1))
+        input_size = input_size // 2
+
+    return drs
+
+def make_model(input_size=64):
+    """Create a DBITNet model.
+
+    :param input_size: e.g. for SPECK32/64 the input_size is 64 bit.
+    :return: DBitNet model.
+    """
+
+    import tensorflow as tf
+    from keras.models import Model
+    from keras.layers import Input, Conv1D, Dense, Dropout, Lambda, concatenate, BatchNormalization, Activation, Add
+    from keras.regularizers import l2
+
+    # determine the dilation rates from the given input size
+    dilation_rates = get_dilation_rates(input_size)
+
+    # prediction head parameters (similar to Gohr)
+    d1 = 256 # TODO this can likely be reduced to 64.
+    d2 = 64
+    reg_param = 1e-5
+
+    # define the input shape
+    inputs = Input(shape=(input_size, 1))
+    x = inputs
+
+    # normalize the input data to a range of [-1, 1]:
+    x = tf.subtract(x, 0.5)
+    x = tf.divide(x, 0.5)
+
+    for dilation_rate in dilation_rates:
+        ### wide-narrow blocks
+        x = Conv1D(filters=n_filters,
+                   kernel_size=2,
+                   padding='valid',
+                   dilation_rate=dilation_rate,
+                   strides=1,
+                   activation='relu')(x)
+        x = BatchNormalization()(x)
+        x_skip = x
+        x = Conv1D(filters=n_filters,
+                   kernel_size=2,
+                   padding='causal',
+                   dilation_rate=1,
+                   activation='relu')(x)
+        x = Add()([x, x_skip])
+        x = BatchNormalization()(x)
+
+        n_filters += n_add_filters
+
+    ### prediction head
+    out = tf.keras.layers.Flatten()(x)
+
+    dense0 = Dense(d1, kernel_regularizer=l2(reg_param))(out);
+    dense0 = BatchNormalization()(dense0);
+    dense0 = Activation('relu')(dense0);
+    dense1 = Dense(d1, kernel_regularizer=l2(reg_param))(dense0);
+    dense1 = BatchNormalization()(dense1);
+    dense1 = Activation('relu')(dense1);
+    dense2 = Dense(d2, kernel_regularizer=l2(reg_param))(dense1);
+    dense2 = BatchNormalization()(dense2);
+    dense2 = Activation('relu')(dense2);
+    out = Dense(1, activation='sigmoid', kernel_regularizer=l2(reg_param))(dense2)
+
+    model = Model(inputs, out)
+
+    return model
+
--- train_nets.py
+++ train_nets.py
@@ -1,9 +1,112 @@
+# TensorFlow setting: Which GPU to use and not to consume the whole GPU:
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'            # Which GPU to use.
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'            # Filters TensorFlow warnings.
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'    # Prevents TensorFlow from consuming the whole GPU.
+
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+import pandas as pd
+
+from .dbitnet import make_model
+
+# CONSTANTS:
+ABORT_TRAINING_BELOW_ACC = 0.5050   # if the validation accuracy reaches or falls below this limit, abort further training.
+EPOCHS = 40                         # train for 40 epochs
+NUM_SAMPLES = 10e6                  # create 10 million training samples
+NUM_VAL_SAMPLES = 1e6               # create 1 million validation samples
+BATCHSIZE = 5000                    # training batch size
+
+def train_one_round(model,
+                    X, Y, X_val, Y_val,
+                    round_number: int,
+                    epochs=40,
+                    load_weight_file=False):
+    """Train the `model` on the training data (X,Y) for one round.
+
+    :param model: TensorFlow neural network
+    :param X, Y: training data
+    :param X_val, Y_val: validation data
+    :param epochs: number of epochs to train
+    :param load_weight_file: Boolean (if True: load weights from previous round.)
+    :return: best validation accuracy
+    """
+    #------------------------------------------------
+    # handle model weight checkpoints
+    #------------------------------------------------
+    from keras.callbacks import ModelCheckpoint
+
+    # load weight checkpoint from previous round?
+    if load_weight_file:
+        model.load_weights(f'results/model_round{round_number}.h5')
+
+    # create model checkpoint callback for this round
+    checkpoint = ModelCheckpoint(f'results/model_round{round_number}.h5', monitor='val_loss', save_best_only = True)
+
+    #------------------------------------------------
+    # train the model
+    #------------------------------------------------
+    history = model.fit(X, Y, epochs=epochs, batch_size=BATCHSIZE,
+                        validation_data=(X_val, Y_val), callbacks=[checkpoint],
+                        verbose=True)
+
+    print("Best validation accuracy: ", np.max(history.history['val_acc']))
+
+    # save the training history
+    pd.to_pickle(history.history, f'results/training_history_round{round_number}.pkl')
+    return np.max(history.history['val_acc'])
+
 def train_neural_distinguisher(starting_round, data_generator):
     """
-
 
     :param starting_round:
     :param data_generator:
     :return: best_round, best_val_acc
     """
+
+    #------------------------------------------------
+    # create the neural network model
+    #------------------------------------------------
+    _X, _Y = data_generator(1, starting_round)  # create a single datapoint to determine the input size
+    input_size = _X.shape[0]                    # determine the input_size from the single datapoint
+    print("DETERMINED INPUT SIZE = ", input_size) # TODO: remove line this once debugged
+    model = make_model(input_size)
+    optimizer = tf.optimizers.Adam(amsgrad=True)
+    model.compile(optimizer=optimizer, loss='mse', metrics=['acc'])
+
+    #------------------------------------------------
+    # start staged training from starting_round
+    #------------------------------------------------
+    current_round = starting_round
+    load_weight_file = False
+    best_val_acc = None
+    best_round = None
+
+    while True:
+
+        # create data
+        X, Y = data_generator(NUM_SAMPLES, current_round)
+        X_val, Y_val = data_generator(NUM_VAL_SAMPLES, current_round)
+
+        # train model for the current round
+        train_one_round(model,
+                        X, Y, X_val, Y_val,
+                        current_round,
+                        epochs = EPOCHS,
+                        load_weight_file = load_weight_file)
+
+        # after the starting_round, load the weight files:
+        load_weight_file = True
+
+        # abort further training if the validation accuracy is too low
+        if val_acc <= ABORT_TRAINING_BELOW_ACC:
+            break
+        # otherwise save results as currently best reached round
+        else:
+            best_round = current_round
+            best_val_acc = val_acc
+            current_round += 1
+
     return best_round, best_val_acc
