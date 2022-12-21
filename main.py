6232a3eaa4e9c121e130da9bc2825dc964ad06f9
davidgerault <david.gerault@tii.ae>
2022-12-21 10:18:58 +0000

    Added PRESENT

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import numpy as np
 
 import optimizer
 #import train_nets
-import speck3264 as cipher
+#import speck3264 as cipher
 #import speck64128 as cipher
 #import speck128256 as cipher
 #import simon3264 as cipher
 #import simon64128 as cipher
 #import simon128256 as cipher
+import present80 as cipher
 
 plain_bits = cipher.plain_bits
 key_bits = cipher.key_bits
